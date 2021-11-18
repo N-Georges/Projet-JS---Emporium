a5787d739d244f27bb7a4f991f353802e5e204c4
georges <gf.nodari@hotmail.com>
2021-11-18 21:15:09 +0100

    hover sur les card pour Up les button de la section recentProduct

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,14 @@
+// scale du logo de la navbar
 let nav = document.querySelector('nav')
 let logo = document.querySelector('.logo')
-let  = document.querySelector('header')
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 160) {
+    if (window.scrollY > 180) {
         logo.classList.add('logoscale')
         logo.classList.remove('logo')
         nav.classList.add('navscale')
 
-    } else if (window.scrollY < 160) {
+    } else if (window.scrollY < 170) {
         logo.classList.remove('logoscale')
         logo.classList.add('logo')
         nav.classList.remove('navscale')
