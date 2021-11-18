503892c72be6551f1c80d5c4d28a5599082f6e8e
georges <gf.nodari@hotmail.com>
2021-11-18 23:57:59 +0100

    style et js du caroussel non finaliser

--- public/js/main.js
+++ public/js/main.js
@@ -14,3 +14,27 @@ window.addEventListener("scroll", () => {
         nav.classList.remove('navscale')
     }
 })
+
+// caroussel
+let dots = document.querySelectorAll('.dot')
+let slides = Array.from(document.querySelectorAll('.carrousel .card')) 
+const SetActive = (i) => {
+    for (slide of slides)
+    if (i == 1) {
+        slide.classList.remove('active')[0];
+        slides[i].classList.add('active')[4];
+    }
+    for(dot of dots)
+    dot.classList.remove('active');
+    dots[i].classList.add('active');
+    console.log(slide)[0];
+    // slides[i].classList.add('active');
+    // ajout de la class active sur les indicateur quand il sont selectionner
+}
+for (let j = 0; j<dots.length; j++){
+    dots[j].addEventListener('click', () => {
+        SetActive(j);
+
+    })
+}
+
