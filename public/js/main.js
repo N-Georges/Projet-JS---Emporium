b0ed672c4c0b3c358f4cceaf5e4f80e4ca99d6fe
georges <gf.nodari@hotmail.com>
2021-11-19 01:05:32 +0100

    carrousel finaliser mais manque effet smooth au scroll

--- public/js/main.js
+++ public/js/main.js
@@ -16,25 +16,82 @@ window.addEventListener("scroll", () => {
 })
 
 // caroussel
-let dots = document.querySelectorAll('.dot')
-let slides = Array.from(document.querySelectorAll('.carrousel .card')) 
-const SetActive = (i) => {
-    for (slide of slides)
-    if (i == 1) {
-        slide.classList.remove('active')[0];
-        slides[i].classList.add('active')[4];
-    }
-    for(dot of dots)
-    dot.classList.remove('active');
-    dots[i].classList.add('active');
-    console.log(slide)[0];
-    // slides[i].classList.add('active');
-    // ajout de la class active sur les indicateur quand il sont selectionner
-}
-for (let j = 0; j<dots.length; j++){
-    dots[j].addEventListener('click', () => {
-        SetActive(j);
+let dots = Array.from(document.querySelectorAll('.dot'))
+let one = dots[0]
+let two = dots[1]
+let three = dots[2]
+let four = dots[3]
+let slides = Array.from(document.querySelectorAll('.carrousel .card'))
+let lSofa = slides[0]
+let gloves = slides[1]
+let bowl = slides[2]
+let bag = slides[3]
+let tshirt = slides[4]
+let set = slides[5]
+let mSofa = slides[6]
+one.addEventListener('click', () => {
+    lSofa.classList.add('active')
+    gloves.classList.add('active')
+    bowl.classList.add('active')
+    bag.classList.add('active')
+    tshirt.classList.remove('active')
+    set.classList.remove('active')
+    mSofa.classList.remove('active')
+    one.classList.add('active')
+    two.classList.remove('active')
+    three.classList.remove('active')
+    four.classList.remove('active')
+    five.classList.remove('active')
 
-    })
-}
+})
+two.addEventListener('click', () => {
+    lSofa.classList.remove('active')
+    tshirt.classList.add('active')
+    one.classList.remove('active')
+    three.classList.remove('active')
+    four.classList.remove('active')
+    two.classList.add('active')
+})
+three.addEventListener('click', () => {
+    lSofa.classList.remove('active')
+    gloves.classList.remove('active')
+    set.classList.add('active')
+    three.classList.add('active')
+    one.classList.remove('active')
+    two.classList.remove('active')
+    four.classList.remove('active')
+    five.classList.remove('active')
+})
+four.addEventListener('click', () => {
+    lSofa.classList.remove('active')
+    gloves.classList.remove('active')
+    bowl.classList.remove('active')
+    mSofa.classList.add('active')
+    three.classList.remove('active')
+    one.classList.remove('active')
+    two.classList.remove('active')
+    four.classList.add('active')
+})
+
+// const SetActive = (i) => {
+//     for (let slide of slides) {
+//         // if (i == 1) {
+//             lSofa.classList.remove('active');
+//             tshirt.classList.add('active');
+//             slides[i].classList.add('active');
+//             console.log(slide)[0];
+//         // }
+//         for (let dot of dots) {
+//             // ajout de la class active sur les indicateur quand il sont selectionner
+//             dot.classList.remove('active');
+//             dots[i].classList.add('active');
+//         }
+//     }
+// }
+// for (let j = 0; j<dots.length; j++){
+//     dots[j].addEventListener('click', () => {
+//         SetActive(j);
+
+//     })
+// }
 
